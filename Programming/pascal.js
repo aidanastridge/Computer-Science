ca2ca55f5c560e9d2e7a52f231b7648d57799f2d
aidanastridge <astridgeaidan@gmail.com>
2025-11-11 15:09:58 -0500

    modified:   Programming/pascal.js

--- Programming/pascal.js
+++ Programming/pascal.js
@@ -1,8 +1,9 @@
-function pascal(n) {
-    if (n === 1) {
-        return 1;
-    }
+function pascal(n,k) {
+  if (k === 0 || k === n) {
+    return 1;}
     else {
-        return pascal(n-1)
+        return pascal(n-1,k-1)+pascal(n-1,k);
     }
 }
+
+console.log(pascal(4,2));
