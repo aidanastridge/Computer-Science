a9d769c0454e79ed6229ecd79bda7baf8686fe3f
aidanastridge <astridgeaidan@gmail.com>
2025-11-14 16:25:41 -0500

    modified:   Programming/fermat.js

--- Programming/fermat.js
+++ Programming/fermat.js
@@ -1,8 +1,26 @@
+function square(x) {
+    return x*x;
+}
+
+function even(n) {
+    return n % 2 === 0;
+}
+
 function expmod(base,exp,m) {
     if (exp === 0) {return 1;}
-    else if (even(exp)) {}
+    else if (even(exp)) {
+        return square(expmod(base,(exp/2),m)) % m;
+    }
+    else {
+        return (base * expmod(base,(exp-1),m)) % m;
+    }
 } 
 
-function even(exp) {
-    return exp === ;
+function fermatTest(n) {
+    function tryIt(a) {
+       if (expmod (a,n,n) === a) {
+        return 
+        }
+    }
 }
+
