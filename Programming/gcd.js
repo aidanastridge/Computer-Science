dca6644699f5296ec197a62383ff1e0455f06938
aidanastridge <astridgeaidan@gmail.com>
2025-11-13 23:05:55 -0500

    modified:   Programming/gcd.js

--- Programming/gcd.js
+++ Programming/gcd.js
@@ -1,6 +1,28 @@
+function square(x) {
+    return x*x;
+}
+
 function gcd(a,b) {
     return b === 0 ? a : gcd(b,(a % b));
 }
 
-console.log(gcd(206,40));
+function smallestDivisor(n) {
+    return findDivisor(n,2);
+}
+
+function findDivisor(n,test_divisor) {
+    if (square(test_divisor) > n) {
+        return n;
+    }
+    else if (divides(test_divisor,n)) {
+        return test_divisor;
+    }
+    else {
+        return findDivisor(n,(test_divisor+1));
+    }
+}
+
+function divides(a,b) {
+    return b % a === 0;
+}
 
