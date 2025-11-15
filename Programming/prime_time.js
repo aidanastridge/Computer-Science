9a8e853886af135361bd6af7f89c1f2d76d61355
aidanastridge <astridgeaidan@gmail.com>
2025-11-15 00:11:47 -0500

    modified:   Programming/prime_time.js

--- Programming/prime_time.js
+++ Programming/prime_time.js
@@ -1,4 +1,47 @@
-function timePrimeTest() {
-    return newline;
+function square(x) {
+    return x*x;
+}
 
+function gcd(a,b) {
+    return b === 0 ? a : gcd(b,(a % b));
+}
+
+function smallestDivisor(n) {
+    return findDivisor(n,2);
+}
+
+function findDivisor(n,test_divisor) {
+    if (square(test_divisor) > n) {
+        return n;
+    }
+    else if (divides(test_divisor,n)) {
+        return test_divisor;
+    }
+    else {
+        return findDivisor(n,(test_divisor+1));
+    }
+}
+
+function divides(a,b) {
+    return b % a === 0;
+}
+
+function prime(n) {
+    return n === smallestDivisor(n);
+}
+
+function timePrimeTest() {
+    console.log("n: ${n}");
+    return startPrimeTest(n,process.uptime());
+}
+
+function startPrimeTest(n, startTime) {
+     if (prime(n)) {
+        reportPrime(process.uptime()-startTime);
+     }
+}
+
+function reportPrime(elapsedTime) {
+    console.log(" *** ");
+    console.log(elapsedTime);
 }
