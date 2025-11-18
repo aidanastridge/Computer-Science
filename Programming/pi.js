f550132785c72e56ab98ef610769cb54cdf3a1ae
aidanastridge <astridgeaidan@gmail.com>
2025-11-17 20:30:42 -0500

    Update pi.js

--- Programming/pi.js
+++ Programming/pi.js
@@ -2,10 +2,8 @@ function piSum(a, b) {
   return a > b ? 0 : ((a + 2) * a) / 1.0 + piSum(a + 4, b);
 }
 
-piSum(2, 6);
-
-function sum(a, b) {
-  return a > b ? 0 : ;
+function sum(term, a, next, b) {
+  return a > b ? 0 : term(a) + sum(term, next(a), next, b);
 }
 
 function inc(n) {
@@ -24,12 +22,35 @@ function identity(x) {
   return x;
 }
 
-function sumIntegers(a,b) {
+function sumIntegers(a, b) {
   return sum(identity(a), inc(b));
 }
 
 function piSum(a, b) {
   function piTerm(x) {
-    return ((x+2)*x)
+    return ((x + 2) * x) / 1.0;
   }
+  function piNext(x) {
+    return x + 4;
+  }
+  return sum(piTerm(a), piNext(b));
 }
+
+(x) => {
+  x + 4;
+};
+
+function piSum(a, b) {
+  return sum(
+    (x) => {
+      return ((x + 2) * x) / 1.0;
+    },
+    a,
+    (x) => {
+      return x + 4;
+    },
+    b,
+  );
+}
+
+
